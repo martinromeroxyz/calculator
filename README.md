41509f727467f9fee7001751f65189ba4333e924
Martín Romero <martinromeroxyz@gmail.com>
2020-05-03 19:16:54 +0200

    Added tracer-1.0.0.jar

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
+
 # Calculator POC
 
 Implementación de un microservicio "calculadora" usando maven + spring-boot (prueba de concepto).
 
-#### Arranque del proyecto
+### Punto de partida
+
+Para empezar he creado un nuevo proyecto Spring-Boot utilizando [spring-initilizr] 
+(https://start.spring.io/) con las dependencias Spring Web, Spring Boot DevTools y Lombok.
+
+### Arranque del proyecto
 Una vez clonado ejecutar en el directorio raíz:
 
 	./mvnw spring-boot:run
--- src/main/java/com/poc/calculator/controller/CalculatorController.java
+++ src/main/java/com/poc/calculator/controller/CalculatorController.java
@@ -4,11 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.corp.calculator.TracerImpl;
+
 @RestController
 public class CalculatorController {
+	
+	TracerImpl tracer = new TracerImpl();
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		
+		tracer.trace("message");
+		
 		return String.format("Hello %s!", name);
 	}
 }
